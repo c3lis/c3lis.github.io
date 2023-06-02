831c1e1f39242a7712b2970fd2a250e46b704275
root <root@localhost.localdomain>
2023-06-02 17:37:41 -0400

    newpost

--- sendel.sh
+++ sendel.sh
@@ -24,20 +24,20 @@ function init(){
 	function listener(){
 		sudo openvpn /home/anonimo/Documentos/tryhackme/vpn/s4ntiagood.ovpn &>/dev/null &
 		jekyll serve --watch --port 80 &>/dev/null & 
-		mkdir /home/anonimo/.www/wwwTryHackMe/imgs/$1 2>/dev/null &>/dev/null && echo -e "\e[33;1mCarpeta Name \e[0m\e[35;1m $1\e[0m";
+		mkdir /home/anonimo/.www/h4cker-0.github.io/imgs/$1 2>/dev/null &>/dev/null && echo -e "\e[33;1mCarpeta Name \e[0m\e[35;1m $1\e[0m";
 	}
 	declare -i parameterCounter=0;
 	function sendel(){
 		parameterCounter=$parameterCounter
 		for x in $(seq 1 10000);do
 			x=$x
-			if test -f /home/anonimo/Captura\ de\ pantalla\ de\ 2023-05-*; then
-				mv /home/anonimo/Captura\ de\ pantalla\ de\ 2023-05-* /home/anonimo/.www/wwwTryHackMe/imgs/$1/$1$parameterCounter.jpg
+			if test -f /home/anonimo/Captura\ de\ pantalla\ de\ 202*; then
+				mv /home/anonimo/Captura\ de\ pantalla\ de\ 202* /home/anonimo/.www/h4cker-0.github.io/imgs/$1/$1$parameterCounter.jpg
 				echo $1$parameterCounter.jpg
 				let parameterCounter+=1
 			else
 				while true;do
-					if test -f /home/anonimo/Captura\ de\ pantalla\ de\ 2023-05-* ; then
+					if test -f /home/anonimo/Captura\ de\ pantalla\ de\ 202* ; then
 						sendel $1
 					fi
 				done
