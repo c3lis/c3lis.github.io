8a4c66ad27337ac8d2a8b86aa3630d267dd86a09
root <root@localhost.localdomain>
2024-10-06 01:33:12 -0400

    Cheese TryHackme add

--- sendel.sh
+++ sendel.sh
@@ -1,1 +1,21 @@
- 
+#!/bin/bash
+function ctrl_c(){
+	echo -e "	* Saliendo"
+	exit 1
+}
+trap ctrl_c INT
+if [ -z $1 ]; then
+	echo -e "* nombre del contenedor de imagenes"
+	exit 1
+fi
+mkdir ./imgs/$1 2>/dev/null
+
+function sendel(){
+
+	counter=0;
+	while true; do
+		mv /home/nouser/Imágenes/2024-* /home/nouser/.www/h4cker-0.github.io/imgs/$1/Cheese$counter.png  2>/dev/null && echo -e "imgs/$1/Cheese$counter.png" | xclip -rmlastnl -sel clip && let counter+=1
+		sleep 2.5
+	done
+}
+sendel $1
